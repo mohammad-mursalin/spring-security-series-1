c6436f901012fa7c8cefd08de3f577da0363ace3
mohammad-mursalin <mursalinlamon@gmail.com>
2024-10-21 10:52:33 +0600

    Initial commit

---
+++ src/main/java/com/mursalin/spring_security_series_1/SpringSecuritySeries1Application.java
@@ -0,0 +1,13 @@
+package com.mursalin.spring_security_series_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuritySeries1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecuritySeries1Application.class, args);
+	}
+
+}
