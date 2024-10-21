83fd4eaf00a45a13d3e4350250693898616d8ec5
mohammad-mursalin <mursalinlamon@gmail.com>
2024-10-21 12:01:40 +0600

    connected to data base

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -1,4 +1,13 @@
 package controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
+
+    @GetMapping("/")
+    public String homePage() {
+        return "Inside home page";
+    }
 }
